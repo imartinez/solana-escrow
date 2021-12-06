d675b2d177b5b1cf0c5cdd39cd1d9dbf8ba83b11
imartinez <ivanmartit@gmail.com>
2021-12-06 13:01:05 +0100

    Started updating state.rs

--- program/src/state.rs
+++ program/src/state.rs
@@ -9,9 +9,8 @@ use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
 pub struct Escrow {
     pub is_initialized: bool,
     pub initializer_pubkey: Pubkey,
-    pub temp_token_account_pubkey: Pubkey,
-    pub initializer_token_to_receive_account_pubkey: Pubkey,
-    pub expected_amount: u64,
+    pub initializer_deposit_account_pubkey: Pubkey,
+    pub deposit_amount: u64,
 }
 
 impl Sealed for Escrow {}
@@ -29,9 +28,9 @@ impl Pack for Escrow {
         let (
             is_initialized,
             initializer_pubkey,
-            temp_token_account_pubkey,
+            initializer_deposit_account_pubkey,
             initializer_token_to_receive_account_pubkey,
-            expected_amount,
+            deposit_amount,
         ) = array_refs![src, 1, 32, 32, 32, 8];
         let is_initialized = match is_initialized {
             [0] => false,
@@ -42,11 +41,11 @@ impl Pack for Escrow {
         Ok(Escrow {
             is_initialized,
             initializer_pubkey: Pubkey::new_from_array(*initializer_pubkey),
-            temp_token_account_pubkey: Pubkey::new_from_array(*temp_token_account_pubkey),
+            initializer_deposit_account_pubkey: Pubkey::new_from_array(*initializer_deposit_account_pubkey),
             initializer_token_to_receive_account_pubkey: Pubkey::new_from_array(
                 *initializer_token_to_receive_account_pubkey,
             ),
-            expected_amount: u64::from_le_bytes(*expected_amount),
+            deposit_amount: u64::from_le_bytes(*deposit_amount),
         })
     }
 
@@ -55,24 +54,24 @@ impl Pack for Escrow {
         let (
             is_initialized_dst,
             initializer_pubkey_dst,
-            temp_token_account_pubkey_dst,
+            initializer_deposit_account_pubkey_dst,
             initializer_token_to_receive_account_pubkey_dst,
-            expected_amount_dst,
+            deposit_amount_dst,
         ) = mut_array_refs![dst, 1, 32, 32, 32, 8];
 
         let Escrow {
             is_initialized,
             initializer_pubkey,
-            temp_token_account_pubkey,
+            initializer_deposit_account_pubkey,
             initializer_token_to_receive_account_pubkey,
-            expected_amount,
+            deposit_amount,
         } = self;
 
         is_initialized_dst[0] = *is_initialized as u8;
         initializer_pubkey_dst.copy_from_slice(initializer_pubkey.as_ref());
-        temp_token_account_pubkey_dst.copy_from_slice(temp_token_account_pubkey.as_ref());
+        initializer_deposit_account_pubkey_dst.copy_from_slice(initializer_deposit_account_pubkey.as_ref());
         initializer_token_to_receive_account_pubkey_dst
             .copy_from_slice(initializer_token_to_receive_account_pubkey.as_ref());
-        *expected_amount_dst = expected_amount.to_le_bytes();
+        *deposit_amount_dst = deposit_amount.to_le_bytes();
     }
 }
